983b32e52db2355e4a3927074388e8426e472928
Matt Brictson <matt@mattbrictson.com>
2024-07-31 17:05:53 -0700

    Consolidate redundant vcr cassettes (#21)
    We were recording a separate vcr cassette for each test case, despite
    the fact that these recordings contain mostly the same requests (i.e.
    looking up changelogs for the gems in our Gemfile fixture).
    
    Consolidate these so that now we use a single VCR cassette. This cuts
    the total file size by half.

--- test/bundle_update_interactive/changelog_locator_test.rb
+++ test/bundle_update_interactive/changelog_locator_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 module BundleUpdateInteractive
   class ChangelogLocatorTest < Minitest::Test
     def test_fetches_changelog_uri_from_rubygems
-      use_vcr_cassette("test_fetches_changelog_uri_from_rubygems") do
+      VCR.use_cassette("changelog_requests") do
         uri = ChangelogLocator.new.find_changelog_uri(name: "nokogiri", version: "1.16.6")
 
         assert_equal "https://nokogiri.org/CHANGELOG.html", uri
@@ -13,7 +13,7 @@ module BundleUpdateInteractive
     end
 
     def test_falls_back_to_top_level_rubygems_data_when_version_does_not_exist
-      use_vcr_cassette("test_falls_back_to_top_level_rubygems_data_when_version_does_not_exist") do
+      VCR.use_cassette("changelog_requests") do
         uri = ChangelogLocator.new.find_changelog_uri(name: "nokogiri", version: "0.123.456")
 
         assert_equal "https://nokogiri.org/CHANGELOG.html", uri
@@ -21,7 +21,7 @@ module BundleUpdateInteractive
     end
 
     def test_discovers_changelog_file_on_github
-      use_vcr_cassette("test_discovers_changelog_file_on_github") do
+      VCR.use_cassette("changelog_requests") do
         # This gem doesn't publish changelog_uri metadata, but does have a GitHub URL for its homepage.
         # We should crawl the GitHub repo and discover that it has a CHANGELOG.md file.
         uri = ChangelogLocator.new.find_changelog_uri(name: "ransack", version: "4.2.0")
@@ -31,7 +31,7 @@ module BundleUpdateInteractive
     end
 
     def test_discovers_changelog_file_on_github_after_following_redirect
-      use_vcr_cassette("test_discovers_changelog_file_on_github_after_following_redirect") do
+      VCR.use_cassette("changelog_requests") do
         # This gem doesn't publish changelog_uri metadata, but does have a GitHub URL for its homepage.
         # However the URL in the metadata in this case still points to seattlerb/minitest, when the
         # repo now lives at minitest/minitest. We should follow the redirect to get the correct URL.
@@ -42,7 +42,7 @@ module BundleUpdateInteractive
     end
 
     def test_discovers_github_releases_url
-      use_vcr_cassette("test_discovers_github_releases_url") do
+      VCR.use_cassette("changelog_requests") do
         # This gem doesn't publish changelog_uri metadata, but does have a GitHub URL for its homepage.
         # The repo doesn't have a CHANGELOG.md file, so we should fall back to GitHub Releases.
         uri = ChangelogLocator.new.find_changelog_uri(name: "web-console", version: "4.2.1")
@@ -52,7 +52,7 @@ module BundleUpdateInteractive
     end
 
     def test_returns_nil_when_changelog_cannot_be_discovered
-      use_vcr_cassette("test_returns_nil_when_changelog_cannot_be_discovered") do
+      VCR.use_cassette("changelog_requests") do
         # This gem doesn't publish changelog_uri metadata, and isn't hosted on GitHub,
         # so we don't have a way to discover its changelog.
         uri = ChangelogLocator.new.find_changelog_uri(name: "atlassian-jwt", version: "0.2.1")
