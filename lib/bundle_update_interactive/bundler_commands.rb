40dd0a3839abe9931fa2651aca12c7fa99ee22f4
Matt Brictson <matt@mattbrictson.com>
2024-08-01 10:46:04 -0700

    Use correct Bundler version when updating gems (#29)
    Before, `update-interactive` would update gems using whatever `bundle`
    binary was in the PATH. On Rails projects, there is often a binstub for
    Bundler located at `bin/bundle`. If this `bin` directory is in the PATH,
    then the binstub is executed instead of Bundler being executed directly.
    
    Using the binstub is problematic, because the binstub locks the version
    of Bundler to the `BUNDLED WITH` value of the lock file. The binstub
    makes an exception for this behavior when running `bin/bundle update`,
    so that the latest version of Bundler is used when updating gems.
    However, this auto-detection only works when the binstub is invoked
    directly in the shell. Since `update-interactive` wraps the invocation
    of the update command, the binstub falls back to locking the version of
    Bundler at whatever is in the lock file.
    
    As a result, `update-interactive` would not update the lockfile with the
    version of Bundler that the user intended.
    
    Fix by using `Gem.bin_path` to get the correct Bundler executable,
    rather than using a binstub that might be in the user's PATH.

--- lib/bundle_update_interactive/bundler_commands.rb
+++ lib/bundle_update_interactive/bundler_commands.rb
@@ -1,21 +1,30 @@
 # frozen_string_literal: true
 
+require "bundler"
 require "shellwords"
 
 module BundleUpdateInteractive
   module BundlerCommands
     class << self
       def update_gems_conservatively(*gems)
-        system "bundle update --conservative #{gems.flatten.map(&:shellescape).join(' ')}"
+        system "#{bundle_bin.shellescape} update --conservative #{gems.flatten.map(&:shellescape).join(' ')}"
       end
 
       def read_updated_lockfile(*gems)
-        command = ["bundle lock --print"]
+        command = ["#{bundle_bin.shellescape} lock --print"]
         command << "--conservative" if gems.any?
         command << "--update"
         command.push(*gems.flatten.map(&:shellescape))
 
         `#{command.join(" ")}`.tap { raise "bundle lock command failed" unless Process.last_status.success? }
+      end
+
+      private
+
+      def bundle_bin
+        Gem.bin_path("bundler", "bundle", Bundler::VERSION)
+      rescue Gem::GemNotFoundException
+        "bundle"
       end
     end
   end
