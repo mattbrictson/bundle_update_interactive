4ff4d9ce53ca25f98ab917cb8aedce5df5f5adca
Matt Brictson <matt@mattbrictson.com>
2024-08-13 14:17:31 -0700

    Add method for parsing `bundle outdated` output

--- test/bundle_update_interactive/bundler_commands_test.rb
+++ test/bundle_update_interactive/bundler_commands_test.rb
@@ -33,11 +33,42 @@ module BundleUpdateInteractive
       assert_match(/bundle lock command failed/i, error.message)
     end
 
+    def test_parse_outdated_returns_hash_of_gem_name_to_newest_version
+      expect_backticks("/exe/bundle outdated --parseable sqlite3 redis-client", captures: <<~STDOUT, success: true)
+
+        redis-client (newest 0.22.2, installed 0.22.1)
+        sqlite3 (newest 2.0.3, installed 1.7.3, requested ~> 1.7)
+      STDOUT
+
+      result = BundlerCommands.parse_outdated("sqlite3", "redis-client")
+      assert_equal(
+        {
+          "redis-client" => "0.22.2",
+          "sqlite3" => "2.0.3"
+        },
+        result
+      )
+    end
+
+    def test_parse_outdated_returns_empty_hash_if_nothing_outdated
+      expect_backticks("/exe/bundle outdated --parseable", captures: "\n", success: true)
+
+      result = BundlerCommands.parse_outdated
+      assert_empty result
+    end
+
+    def test_parse_outdated_raises_if_bundle_command_fails_with_no_output
+      expect_backticks("/exe/bundle outdated --parseable", captures: "", success: false)
+
+      error = assert_raises(RuntimeError) { BundlerCommands.parse_outdated }
+      assert_match(/bundle outdated command failed/i, error.message)
+    end
+
     private
 
     def expect_backticks(command, captures: "", success: true)
-      status = mock(success?: success)
-      Process.expects(:last_status).returns(status)
+      status = stub(success?: success)
+      Process.stubs(:last_status).returns(status)
       BundlerCommands.expects(:`).with(command).returns(captures)
     end
   end
