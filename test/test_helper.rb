18daf3c194112652dd845b033bd4803b510d042d
Matt Brictson <matt@mattbrictson.com>
2024-08-20 15:30:00 -0700

    Make mocha configuration more strict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bundle_update_interactive"
 require "minitest/autorun"
-require "mocha/minitest"
 
 BundleUpdateInteractive.pastel = Pastel.new(enabled: true)
 
