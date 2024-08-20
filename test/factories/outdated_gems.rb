bee88601831d638b66c35e1de5bf845cd7473d4c
Matt Brictson <matt@mattbrictson.com>
2024-08-20 15:25:48 -0700

    Introduce a true integration test

--- test/factories/outdated_gems.rb
+++ test/factories/outdated_gems.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :outdated_gem, class: "BundleUpdateInteractive::OutdatedGem" do
     current_git_version { nil }
-    current_version { "7.0.3" }
+    current_version { "0.0.1" }
     git_source_uri { nil }
     name { "rails" }
     rubygems_source { true }
