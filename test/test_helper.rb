050c9daf18f12d3003cf31f419205dbce4015ea7
pacMakaveli <pacMakaveli90@gmail.com>
2024-08-22 08:55:27 +0100

    chore: remove activesupport requires from the test helper and fix rubocop complaint

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,6 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bundle_update_interactive"
 require "minitest/autorun"
 
-require 'active_support/core_ext/module/delegation'
-require 'active_support/inflector'
-
 BundleUpdateInteractive.pastel = Pastel.new(enabled: true)
 
 Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |rb| require(rb) }
